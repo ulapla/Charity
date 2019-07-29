0742c6452d72fa40535e19be15fcbfb744007c25
Ula Placek <ulaplacek@gmail.com>
2019-07-29 10:26:54 +0200

    entities created

---
+++ src/main/java/pl/coderslab/charity/category/CategoryService.java
@@ -0,0 +1,13 @@
+package pl.coderslab.charity.category;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+}
---
+++ src/main/java/pl/coderslab/charity/donation/Donation.java
@@ -0,0 +1,32 @@
+package pl.coderslab.charity.donation;
+
+import lombok.Data;
+import pl.coderslab.charity.category.Category;
+import pl.coderslab.charity.institution.Institution;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
+@Data
+public class Donation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int quantity;
+    @OneToMany
+    private List<Category> categories;
+    @ManyToOne
+    private Institution institution;
+    private String street;
+    private String city;
+    private String zipCode;
+    private LocalDate pickUpDate;
+    private LocalTime pickUpTime;
+    private String pickUpComment;
+
+
+}
