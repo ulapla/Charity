2cd9b999b4807898af61f6e71aa8a9712a2b57b7
Ula Placek <ulaplacek@gmail.com>
2019-08-22 22:43:34 +0200

    sending email from contact section added

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -2,11 +2,12 @@ package pl.coderslab.charity;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.coderslab.charity.donation.DonationService;
+import pl.coderslab.charity.email.EmailServiceImpl;
 import pl.coderslab.charity.institution.InstitutionService;
-import pl.coderslab.charity.user.User;
 
 
 @Controller
@@ -14,10 +15,12 @@ public class HomeController {
 
     private InstitutionService institutionService;
     private DonationService donationService;
+    private EmailServiceImpl emailService;
 
-    public HomeController(InstitutionService institutionService, DonationService donationService) {
+    public HomeController(InstitutionService institutionService, DonationService donationService, EmailServiceImpl emailService) {
         this.institutionService = institutionService;
         this.donationService = donationService;
+        this.emailService = emailService;
     }
 
     @RequestMapping("/")
@@ -26,6 +29,13 @@ public class HomeController {
         model.addAttribute("allDonations", donationService.allDonationQuantity());
         model.addAttribute("supportedInstitutions", donationService.countAllInstitutions());
         return "index";
+    }
+
+    @PostMapping("/")
+    public String sentEmail(@RequestParam String name, @RequestParam String surname, @RequestParam String message){
+        emailService.sendSimpleMessage("ulaplacek@gmail.com","Wiadomość od "+ name + " "+ surname, message);
+        System.out.println("dziala");
+        return "redirect:/";
     }
 
 
--- src/main/webapp/WEB-INF/views/footer.jsp
+++ src/main/webapp/WEB-INF/views/footer.jsp
@@ -1,18 +1,18 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <footer>
-<%--    <div id="contact" class="contact">--%>
-<%--        <h2>Skontaktuj się z nami</h2>--%>
-<%--        <h3>Formularz kontaktowy</h3>--%>
-<%--        <form class="form--contact">--%>
-<%--            <div class="form-group form-group--50"><input type="text" name="name" placeholder="Imię"/></div>--%>
-<%--            <div class="form-group form-group--50"><input type="text" name="surname" placeholder="Nazwisko"/></div>--%>
+    <div id="contact" class="contact">
+        <h2>Skontaktuj się z nami</h2>
+        <h3>Formularz kontaktowy</h3>
+        <form class="form--contact" method="post" action="/">
+            <div class="form-group form-group--50"><input type="text" name="name" placeholder="Imię"/></div>
+            <div class="form-group form-group--50"><input type="text" name="surname" placeholder="Nazwisko"/></div>
 
-<%--            <div class="form-group"><textarea name="message" placeholder="Wiadomość" rows="1"></textarea></div>--%>
+            <div class="form-group"><textarea name="message" placeholder="Wiadomość" rows="1"></textarea></div>
 
-<%--            <button class="btn" type="submit">Wyślij</button>--%>
-<%--        </form>--%>
-<%--    </div>--%>
+            <button class="btn" type="submit">Wyślij</button>
+        </form>
+    </div>
     <div class="bottom-line">
         <span class="bottom-line--copy">Copyright &copy; 2018</span>
         <div class="bottom-line--icons">
